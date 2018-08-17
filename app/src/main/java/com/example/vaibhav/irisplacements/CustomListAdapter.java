411aeccc17de9514a26d8bc510501b19b708e164
Vaibhav G <gvaibhav1734@gmail.com>
2018-08-18 00:19:58 +0530

    Volley requests implemented. CardView added. Colors, strings and styles updated. saveInstanceState implemented. Bugs squashed.

--- app/src/main/java/com/example/vaibhav/irisplacements/CustomListAdapter.java
+++ app/src/main/java/com/example/vaibhav/irisplacements/CustomListAdapter.java
@@ -3,25 +3,38 @@ package com.example.vaibhav.irisplacements;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class CustomListAdapter extends RecyclerView.Adapter<CustomListAdapter.ViewHolder>{
+public class CustomListAdapter extends RecyclerView.Adapter<CustomListAdapter.ViewHolder> {
+    private static final String TAG = "CustomListAdapter";
     private Context mContext;
-    private List<Entry> entries;
-    CustomListAdapter(Context context){
+    private List<Entry> entries = new ArrayList<>();
+
+    CustomListAdapter(Context context) {
         this.mContext = context;
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder{
-        TextView company_name,recruitment_date,deadline;
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView company_name, recruitment_date, deadline;
         Button apply;
-        ViewHolder(View itemView){
+
+        ViewHolder(View itemView) {
             super(itemView);
             company_name = itemView.findViewById(R.id.list_item_tv_company_name);
             recruitment_date = itemView.findViewById(R.id.list_item_tv_recruitment_date);
@@ -29,6 +42,7 @@ public class CustomListAdapter extends RecyclerView.Adapter<CustomListAdapter.Vi
             apply = itemView.findViewById(R.id.list_item_btn_apply);
         }
     }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -39,20 +53,64 @@ public class CustomListAdapter extends RecyclerView.Adapter<CustomListAdapter.Vi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        Log.d(TAG, entries.get(position).getUrl());
         holder.company_name.setText(entries.get(position).getCompany().getName());
         holder.recruitment_date.setText(entries.get(position).getRecruitment_date());
         holder.deadline.setText(entries.get(position).getDeadline());
         holder.apply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO:Volley request.
+                JSONObject json = null;
+                try {
+                    json = new JSONObject();
+                    json.put("commit", "true");
+                } catch (JSONException error) {
+                    Log.e(TAG, "commit JSON creation error : " + error.getMessage());
+                }
+                String url = mContext.getString(R.string.POST_APPLICATION);
+                JsonObjectRequest commitRequest = new JsonObjectRequest(
+                        Request.Method.POST,
+                        url,
+                        json,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.d(TAG, "Volley commitRequest success : " + response.toString());
+                                MainActivity instance = (MainActivity) mContext;
+                                instance.addToApplicationsList(
+                                        entries.get(holder.getAdapterPosition())
+                                );
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }
+                );
+                VolleyHelper.getInstance(mContext).addToRequestQueue(commitRequest);
             }
         });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return entries.size();
+    }
+
+
+    public void setEntries(List<Entry> entries) {
+        this.entries = entries;
+    }
+
+    public List<Entry> getEntries() {
+        return entries;
+    }
+
+    public void addEntry(Entry entry) {
+        entries.add(entry);
+        notifyItemInserted(entries.size() - 1);
     }
 }
--- app/src/main/java/com/example/vaibhav/irisplacements/MainActivity.java
+++ app/src/main/java/com/example/vaibhav/irisplacements/MainActivity.java
@@ -7,14 +7,41 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private ViewPager mViewPager;
+    private CustomListAdapter mCompaniesListAdapter;
+    private CustomListAdapter mApplicationsListAdapter;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save only applications list.
+        outState.putParcelableArrayList(
+                "applicationsList",
+                (ArrayList<Entry>) mApplicationsListAdapter.getEntries()
+        );
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +51,26 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = findViewById(R.id.main_vp_list_container);
         tabLayout.setupWithViewPager(mViewPager);
         mViewPager.setAdapter(new CustomPagerAdapter());
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        toolbar.setTitle(R.string.app_name);
+        mApplicationsListAdapter = new CustomListAdapter(this);
+        if (savedInstanceState != null) {
+            List<Entry> ae = savedInstanceState.getParcelableArrayList("applicationsList");
+            mApplicationsListAdapter.setEntries(ae);
+        }
     }
-    class CustomPagerAdapter extends PagerAdapter{
+
+    public void addToApplicationsList(Entry entry) {
+        mApplicationsListAdapter.addEntry(entry);
+    }
+
+    class CustomPagerAdapter extends PagerAdapter {
         public static final int COMPANIES = 0;
         public static final int APPLICATIONS = 1;
-        private String titles[] = {"Companies","Applications"};
-        private int layouts[] = {R.layout.layout_companies,R.layout.layout_applications};
+        private String titles[] = {"Companies", "Applications"};
+        private int layouts[] = {R.layout.layout_companies, R.layout.layout_applications};
 
-        CustomPagerAdapter(){
+        CustomPagerAdapter() {
         }
 
         @Override
@@ -43,21 +82,55 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public Object instantiateItem(@NonNull ViewGroup container, int position) {
             LayoutInflater layoutInflater = getLayoutInflater();
-            View rootView = layoutInflater.inflate(layouts[position],container,false);
+            View rootView = layoutInflater.inflate(layouts[position], container, false);
             container.addView(rootView);
-            RecyclerView recyclerView;
-            if(position==COMPANIES){
-                recyclerView = findViewById(R.id.companies_rv_list);
+            final RecyclerView recyclerView;
+            if (position == COMPANIES) {
+                String url = getString(R.string.GET_COMPANIES);
+                recyclerView = rootView.findViewById(R.id.companies_rv_list);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                JsonArrayRequest companiesRequest = new JsonArrayRequest(
+                        Request.Method.GET,
+                        url,
+                        null,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                Log.d(TAG, "Volley success : " + response.toString());
+                                Type listType = new TypeToken<ArrayList<Entry>>() {
+                                }.getType();
+                                ArrayList<Entry> entries =
+                                        GsonHelper.getInstance()
+                                                .getGson().fromJson(response.toString(), listType);
+                                Log.d(TAG, "List after GSON : " + entries.toString());
+                                mCompaniesListAdapter =
+                                        new CustomListAdapter(MainActivity.this);
+                                mCompaniesListAdapter.setEntries(entries);
+                                recyclerView.setAdapter(mCompaniesListAdapter);
+                                mCompaniesListAdapter.notifyDataSetChanged();
+                                Log.d(TAG, "Size : " + mCompaniesListAdapter.getItemCount());
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.e(TAG, "Volley companiesRequest error : " + error.getMessage());
+                            }
+                        }
+                );
+                VolleyHelper.getInstance(getApplicationContext())
+                        .addToRequestQueue(companiesRequest);
             } else {
-                recyclerView = findViewById(R.id.applications_rv_list);
+                recyclerView = rootView.findViewById(R.id.applications_rv_list);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                recyclerView.setAdapter(mApplicationsListAdapter);
             }
-            recyclerView.setAdapter(new CustomListAdapter(getApplicationContext()));
-            return container;
+            return rootView;
         }
 
         @Override
         public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-            super.destroyItem(container, position, object);
+            container.removeView((View) object);
         }
 
         @Override
